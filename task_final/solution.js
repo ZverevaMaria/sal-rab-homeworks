614fc181239054cba38d7210ae07d71f901bdf52
Zvereva Maria <masha.voronova.93@mail.ru>
2023-04-06 19:36:38 +0500

    final task complete

--- task_final/solution.js
+++ task_final/solution.js
@@ -45,7 +45,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.sum = sum;
     data.client = name + ' ' + phone;
     let jsonData = JSON.stringify({data});
-
+    
     return jsonData;
 }
 
